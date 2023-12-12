7cc12fbf18dc42a03ffa0e899bcb10b9594be389
Jay <jay954340@gmail.com>
2023-12-12 21:36:49 +0530

    add loop

---
+++ loop.java
@@ -0,0 +1,19 @@
+public class loop {
+//loop method
+    public static void main (String[] args){
+        int i=0;
+        while (i<10) {
+            System.out.println(i);
+            i++;
+        }
+        i=0;
+       for (  i=0; i<10; i++) {
+           System.out.println(i);
+       }
+       i=0;
+        do {
+            System.out.println(i);
+            i++;
+       } while (i<10);
+    }
+}
