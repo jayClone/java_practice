624f50d2f2418735f5efa9d4bb62fe348e758490
Jay <jay954340@gmail.com>
2023-12-12 22:11:43 +0530

    add loop

--- loop.java
+++ loop.java
@@ -6,11 +6,11 @@ public class loop {
             System.out.println(i);
             i++;
         }
-        i=0;
+        i=0;// reset i
        for (  i=0; i<10; i++) {
            System.out.println(i);
        }
-       i=0;
+       i=0;//reset i
         do {
             System.out.println(i);
             i++;
