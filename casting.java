61b0abd9fafa9366928baa7f89f5ea595419e365
Jay <jay954340@gmail.com>
2023-12-12 22:29:19 +0530

    type casting

---
+++ casting.java
@@ -0,0 +1,13 @@
+public class casting {
+    //casting method
+    public static void main(String[] args){
+        //implicit casting
+        int i=10;
+        double d=i;
+        System.out.println("i="+i+" d="+d);
+        //explicit casting
+        double d2=10.5;
+        int i2=(int)d2;
+        System.out.println("d2="+d2+" i2="+i2);
+    }
+}
